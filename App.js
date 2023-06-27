e3da1e8af01f1e1a6b9fb104f2bd034531c60e69
Bryan Rodriguez <Serphp@hotmail.com>
2023-06-27 14:19:41 -0500

    RN + Expo Init

--- App.js
+++ App.js
@@ -1,14 +1,30 @@
+
+import * as React from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { PaperProvider } from 'react-native-paper';
+
+const theme = {
+  ...DefaultTheme,
+  colors: {
+    ...DefaultTheme.colors,
+    primary: 'tomato',
+    secondary: 'yellow',
+  },
+};
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <PaperProvider theme={theme}>
+      <View style={styles.container}>
+        <Text>Open up App.js to start working on your app!</Text>
+        <StatusBar style="auto" />
+      </View>
+    </PaperProvider>
   );
 }
+
+
 
 const styles = StyleSheet.create({
   container: {
