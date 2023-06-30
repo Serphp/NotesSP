4ce63b6524280c2600366aa47c20e2a72cfb4d11
Bryan Rodriguez <Serphp@hotmail.com>
2023-06-30 18:26:35 -0500

    add: new style navbar & menu & add note

--- App.js
+++ App.js
@@ -1,10 +1,13 @@
 import * as React from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import { DefaultTheme, Provider as PaperProvider } from 'react-native-paper';
+import { DefaultTheme, List, Provider as PaperProvider } from 'react-native-paper';
 import Home from './screen/Home';
+import About from './screen/About';
+import Nota from './screen/Nota';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+//import { BlurView } from 'expo-blur';
 //import { Router, Route , hashHistory } from 'react-router';
 
 const theme = {
@@ -27,7 +30,41 @@ export default function App() {
       </View> */}
       <NavigationContainer>
         <Stack.Navigator initialRouteName="Home">
-          <Stack.Screen name="Home" component={Home} options={{ title: 'Overview' }}/>
+          <Stack.Screen name="Home" component={Home} options={{
+            //headerTransparent: true,
+            headerTitle: 'SPnotas',
+            headerTitleStyle: {
+              fontWeight: 'bold',
+              fontSize: 25,
+              color: 'white',
+            },
+            headerStyle: {
+              backgroundColor: 'tomato',
+              height: 60,
+            },
+            headerRight: (props) => (
+              <List.Icon
+                icon={'plus'}
+                style={{ marginRight: 20, color: 'white'}}
+                {...props}
+                onPress={() => {
+                  // Do something
+                }}
+              />
+            ),
+            headerLeft: (props) => (
+            <List.Icon
+              icon={'menu'}
+              style={{ marginLeft: 20, color: 'white'}}
+              {...props}
+              onPress={() => {
+                // Do something
+              }}
+            />
+    ),
+  }}/>
+          <Stack.Screen name="About" component={About} />
+          {/* <Stack.Screen name="Nota" component={Nota} /> */}
         </Stack.Navigator>
       </NavigationContainer>
     </PaperProvider>
